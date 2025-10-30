f2961c24e69fe09425c574cf41247426bb95a92a
Fedor Kobyakov <fedor.kobyakov@onlyoffice.com>
2025-10-30 13:27:48 +0300

    remove useless components and create global styles for tags

--- src/components/Pages/Registration/Registration.tsx
+++ src/components/Pages/Registration/Registration.tsx
@@ -1,29 +1,28 @@
-import Form from "../../Form/index.ts";
-import Button from "../../Button/index.ts";
-import H1 from "../../H1/index.ts";
+import { Link } from "react-router-dom";
 import TextInput from "../../TextInput/TextInput.tsx";
-
-import styles from "./Registration.module.css";
 import Checkbox from "../../Checkbox/Checkbox.tsx";
-import Span from "../../Span/index.ts";
-import ALink from "../../ALink/ALink.tsx";
+import styles from "./Registration.module.css";
 
 
 const Registration = () => {
     return (
         <div className={styles.container}>
-            <Form>
-                <H1 className={styles.formHead}>Hello</H1>
+            <div className="form">
+                <h1 className={styles.formHead}>Hello</h1>
                 <div className={styles.mainContent}>
                     <TextInput placeholder="Введите почту" label="Корпоративный e-mail" />
-                    <Checkbox onChange={() => {}}><Span>Я подтверждаю согласие с <ALink to={"#"}>политикой конфиденциальности</ALink></Span></Checkbox>
-                    <Button variant="primary">Продолжить</Button>
+                    <Checkbox onChange={() => {}}>
+                        <span>Я подтверждаю согласие с <Link to="#">политикой конфиденциальности</Link></span>
+                    </Checkbox>
+                    <button className="primary">Продолжить</button>
                 </div>
                 <div className={styles.asideContent}>
-                    <Button variant="secondary">Войти</Button>
-                    <Span className={styles.helpText}>Возник вопрос или что-то сломалось? <ALink to="#">Вступай в чат и задавай вопрос</ALink></Span>
+                    <button className="secondary">Войти</button>
+                    <span className={styles.helpText}>
+                        Возник вопрос или что-то сломалось? <Link to="#">Вступай в чат и задавай вопрос</Link>
+                    </span>
                 </div>
-            </Form>
+            </div>
         </div>
     )
 }
